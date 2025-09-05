c2fb3f10a71880507899cdd09d346c27cf4e3be4
dobshn <158257259+dobshn@users.noreply.github.com>
2025-09-05 21:54:29 +0900

    fix: 암호문 바이너리가 탭, 개행 문자를 포함할 수 있는 버그 수정
    - 암호문이 base64 포맷으로 인코딩되어 탭, 개행 문자가 포함되지 않도록 수정

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <cryptopp/aes.h>
 #include <cryptopp/modes.h>
 #include <cryptopp/filters.h>
+#include <cryptopp/base64.h>
 
 using namespace std;
 
@@ -22,22 +23,29 @@ CryptoPP::byte iv[CryptoPP::AES::BLOCKSIZE] = {0};
  * 평문을 입력받아 전역 key와 iv를 사용해 암호화된 문자열을 반환한다.
  */
 string encrypt(const string& plain) {
+    // 1) AES-CBC로 암호화
     string cipher;
-
     CryptoPP::CBC_Mode<CryptoPP::AES>::Encryption enc;
     enc.SetKeyWithIV(key, sizeof(key), iv);
-    // plain 문자열을 enc 필터를 거쳐 cipher에 저장한다.
     CryptoPP::StringSource ss1(plain, true, new CryptoPP::StreamTransformationFilter(enc, new CryptoPP::StringSink(cipher)));
 
-    return cipher;
+    // 2) 바이너리를 Base64로 변환
+    string cipher_b64;
+    CryptoPP::StringSource ss2(cipher, true, new CryptoPP::Base64Encoder(new CryptoPP::StringSink(cipher_b64), false));
+
+    return cipher_b64;
 }
 
 /**
  * 암호문을 입력받아 전역 key와 iv를 사용해 복호화된 문자열을 반환한다.
  */
-string decrypt(const string& cipher) {
-    string plain;
+string decrypt(const string& cipher_b64) {
+    // 1) Base64를 바이너리로 변환
+    string cipher;
+    CryptoPP::StringSource ss1(cipher_b64, true, new CryptoPP::Base64Decoder(new CryptoPP::StringSink(cipher)));
 
+    // 2) AES-CBC로 복호화
+    string plain;
     CryptoPP::CBC_Mode<CryptoPP::AES>::Decryption dec;
     dec.SetKeyWithIV(key, sizeof(key), iv);
     CryptoPP::StringSource ss2(cipher, true, new CryptoPP::StreamTransformationFilter(dec, new CryptoPP::StringSink(plain)));
